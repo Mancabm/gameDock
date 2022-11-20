6ebcfd6c48d6b5d6916c833cab176b0c4a37cdad
Juan Lopez <juanquiroslopez@gmail.com>
2022-11-20 18:35:19 +0100

    start feature

--- GameDock/urls.py
+++ GameDock/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from gameDockApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/product/<int:id_product>', views.product_detail)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gameDockApp/views.py
+++ gameDockApp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
 from gameDockApp.models import Producto
 
 #muestra los títulos de los productos que están registrados
 def clientePrincipal(request):
     productos=Producto.objects.all()
     return render(request,'clientePrincipal.html', {'productos':productos})
+
+def product_detail(request, id_producto):
+    producto = get_object_or_404(Producto, pk=id_producto)
+    return render(request, 'product_detail.html',{'producto': producto, 'MEDIA_URL': settings.MEDIA_URL})
