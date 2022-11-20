6cecdcc7e2a3785d20679d0d61a3fa62c2e654cb
joscasvaz <jicastrotic99@gmail.com>
2022-11-20 12:26:23 +0100

    Update settings.py

--- GameDock/settings.py
+++ GameDock/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whoosh',
+    'haystack',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+WHOOSH_INDEX = os.path.join(BASE_DIR, 'whoosh/')
+
+HAYSTACK_CONNECTIONS = {
+  'default':{
+      'ENGINE':'haystack.backends.whoosh_backend.WhooshEngine',
+      'PATH': WHOOSH_INDEX,
+  },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
