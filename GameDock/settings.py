c50054ebf174d6897f1a456c720e0487034f8114
santizdr <santizuleta11@gmail.com>
2022-12-05 11:56:59 +0100

    feat: mejoras en la UX

--- GameDock/settings.py
+++ GameDock/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gameDockApp',
-    'bootstrap5'
+    'bootstrap4', 
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
